5475e57f00b765d72aa3db71f4176b705e926ecf
Optimisoptimus <63745797+Codeomicronzeta@users.noreply.github.com>
2021-10-13 18:36:04 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,9 @@ from werkzeug.serving import run_simple
 
 from app import server as flask_app
 from dashboard import app as app1
+from data_ops import app as app2
 
 application = DispatcherMiddleware(flask_app, {
     '/app1': app_1.server
+    '/app2': app_2.server
 }) 
