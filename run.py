f4a8498ff15862d2817187f90bb193f04991ddcf
Optimisoptimus <63745797+Codeomicronzeta@users.noreply.github.com>
2021-10-13 18:37:00 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,11 +3,12 @@ from werkzeug.serving import run_simple
 
 from dashboard import app as app1
 from app import server as flask_app
-
+from data_ops import app as app2
 
 application = DispatcherMiddleware(flask_app, {
     '/app1': app1.server,
+    '/app2': app2.server
 })
 
 if __name__ == '__main__':
-    run_simple('localhost', 8050, application) 
+    run_simple('localhost', "user_port_no", application) 
